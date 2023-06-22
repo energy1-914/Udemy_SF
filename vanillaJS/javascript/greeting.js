a4f13696cd175c87f7a492f55fc2f95d2e67752f
energy1-914 <dlawldnjs5@daum.net>
2023-06-22 23:32:31 +0900

    3주차 개인 과제 - 인사 기능 추가

---
+++ vanillaJS/javascript/greeting.js
@@ -0,0 +1,9 @@
+const getName = () => {
+  const name = document.querySelector(".name").value;
+  const greeting = document.querySelector(".greeting");
+  const hour = new Date().getHours();
+  const text =
+    hour < 11 ? "Good morning!" : hour < 17 ? "Good Afternoon!" : "Good evening!";
+  greeting.innerHTML = `<h3>${text} ${name}</h3>`;
+};
+document.querySelector(".button").addEventListener("click", getName);
