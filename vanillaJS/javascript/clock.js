889ad92495ecff0266f98ed4d0183dfc70bfbaec
energy1-914 <dlawldnjs5@daum.net>
2023-06-23 01:21:19 +0900

    chores

--- vanillaJS/javascript/clock.js
+++ vanillaJS/javascript/clock.js
@@ -4,8 +4,7 @@ const getTime = ()=>{
   const date = new Date();
   const hour = date.getHours();
   const minute = date.getMinutes();
-  const se = date.getSeconds();
-  clock.innerHTML = `${hour} : ${minute}`;
+  clock.innerHTML = `${hour}시 ${minute}분`;
 }
 getTime();
 setInterval(getTime, 60000);
--- vanillaJS/javascript/greeting.js
+++ vanillaJS/javascript/greeting.js
@@ -3,7 +3,11 @@ const getName = () => {
   const greeting = document.querySelector(".greeting");
   const hour = new Date().getHours();
   const text =
-    hour < 11 ? "Good morning!" : hour < 17 ? "Good Afternoon!" : "Good evening!";
+    hour < 11 && hour >= 5
+      ? "Good morning!"
+      :hour >= 11 && hour < 17
+      ? "Good Afternoon!"
+      : "Good evening!";
   greeting.innerHTML = `<h3>${text} ${name}</h3>`;
 };
 document.querySelector(".button").addEventListener("click", getName);
